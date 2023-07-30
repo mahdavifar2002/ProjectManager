fb1272fbcdbe12126dd7b81a6b326ae51100ba08
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-07-30 10:23:29 +0330

    Add full whl files installer and change developer's username to pc25

---
+++ WHL/WHL.py
@@ -0,0 +1,27 @@
+import subprocess
+
+# First, install 'tqdm', the package for showing progress of python loop
+WHL = ['basics\\tqdm-4.65.0-py3-none-any.whl',
+       'basics\\colorama-0.4.6-py2.py3-none-any.whl']
+
+print("Please wait for start of the process.")
+for item in WHL:
+    subprocess.call(["pip", "install", "--no-deps", "--no-index", str(item)], stdout=subprocess.DEVNULL)
+
+# second, install dependency packages of the project
+
+WHL = ['greenlet-2.0.2-cp37-cp37m-win_amd64.whl',
+       'importlib_metadata-6.7.0-py3-none-any.whl',
+       'jdatetime-4.1.1-py3-none-any.whl',
+       'mysqlclient-2.1.0-cp37-cp37m-win_amd64.whl',
+       'PySide6-6.2.4-6.2.4-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl',
+       'shiboken6-6.2.4-6.2.4-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl',
+       'SQLAlchemy-2.0.19-cp37-cp37m-win_amd64.whl',
+       'typing_extensions-4.7.1-py3-none-any.whl',
+       'zipp-3.15.0-py3-none-any.whl']
+
+print("Installing packages...")
+from tqdm import tqdm
+for item in tqdm(WHL):
+    subprocess.call(["pip", "install", "--no-deps", "--no-index", str(item)], stdout=subprocess.DEVNULL)
+
--- db_init.py
+++ db_init.py
@@ -12,11 +12,11 @@ from model.user import User
 # conf.drop_db()
 conf.init_db()
 
-user = User(username="mahdavifar", password="96321", fullname="علی مهدوی فر", image_path="\\\\khakbaz\\E\\ProjectManager\\WorksManager\\Pic\\FMahdavifar.jpg")
-user.save()
+# user = User(username="mahdavifar", password="96321", fullname="علی مهدوی فر", image_path="\\\\khakbaz\\E\\ProjectManager\\WorksManager\\Pic\\FMahdavifar.jpg")
+# user.save()
 
 # Load users from Works Manager
-accounts_path = pathlib.Path(QDir.toNativeSeparators("//khakbaz/E/ProjectManager/WorksManager/List/Account.lst"))
+accounts_path = pathlib.Path(QDir.toNativeSeparators("//alireza/E/Works Manager/List/Account.lst"))
 
 with open(str(accounts_path), mode="r", encoding="utf-8") as accounts_file:
     accounts_str = accounts_file.read().replace('\'', "\"")
@@ -26,7 +26,8 @@ with open(str(accounts_path), mode="r", encoding="utf-8") as accounts_file:
         account[3] = "F" + account[3][1:]
 
         fullname = account[0] + " " + account[1]
-        username = account[2].lower()
+        # username = account[2].lower()
+        username = account[3][1:].lower()
         image_path = str(pathlib.Path(QDir.toNativeSeparators(f"//khakbaz/E/ProjectManager/WorksManager/Pic/{account[3]}.png")))
 
         user = User(username=username, password="96321", fullname=fullname, image_path=image_path)
@@ -36,17 +37,17 @@ with open(str(accounts_path), mode="r", encoding="utf-8") as accounts_file:
 
 
 # Add dummy rows for messages and tasks table
-message1 = Message(sender_username="alireza", receiver_username="mahdavifar", text="Hello! How are you?")
+message1 = Message(sender_username="alireza", receiver_username="pc25", text="Hello! How are you?")
 message1.save()
-message2 = Message(sender_username="mahdavifar", receiver_username="alireza", text="I'm fine, thank you.", reply_to=message1.id)
+message2 = Message(sender_username="pc25", receiver_username="alireza", text="I'm fine, thank you.", reply_to=message1.id)
 message2.save()
 
 for i in range(10):
-    message1 = Message(sender_username="alireza", receiver_username="mahdavifar", text=str(2*i + 1))
+    message1 = Message(sender_username="alireza", receiver_username="pc25", text=str(2*i + 1))
     message1.save()
-    message2 = Message(sender_username="mahdavifar", receiver_username="alireza", text=str(2*i + 2),
+    message2 = Message(sender_username="pc25", receiver_username="alireza", text=str(2*i + 2),
                        reply_to=message1.id)
     message2.save()
 
-task = Task(assigner_username="alireza", assignee_username="mahdavifar", description="Creating project management program.")
+task = Task(assigner_username="alireza", assignee_username="pc25", description="Creating project management program.")
 task.save()
