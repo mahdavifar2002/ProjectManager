6cfee75a6a6cdfc927abde58c47e69e441a7c6ce
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-08-26 12:04:25 +0330

    Increase timeout of pinging from 0.1s to 0.2s to avoid false negative

--- modules/client.py
+++ modules/client.py
@@ -26,7 +26,7 @@ def check_share(share_name):
     t = threading.Thread(target=check_share_lazy, args=(share_name,))
     t.daemon = True
     t.start()
-    timeout_seconds = 0.1  # Replace with your desired timeout value in seconds
+    timeout_seconds = 0.2  # Replace with your desired timeout value in seconds
     t.join(timeout_seconds)  # Set the timeout for the thread's runtime
 
     if t.is_alive():
@@ -38,6 +38,7 @@ def check_share(share_name):
 def check_online_lazy(share_name):
     import socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.2)
     result = sock.connect_ex((share_name, 50051))
     sock.close()
     if result == 0:
@@ -50,7 +51,7 @@ def check_online(share_name):
     t = threading.Thread(target=check_online_lazy, args=(share_name,))
     t.daemon = True
     t.start()
-    timeout_seconds = 0.1  # Replace with your desired timeout value in seconds
+    timeout_seconds = 0.2  # Replace with your desired timeout value in seconds
     t.join(timeout_seconds)  # Set the timeout for the thread's runtime
 
     if t.is_alive():
